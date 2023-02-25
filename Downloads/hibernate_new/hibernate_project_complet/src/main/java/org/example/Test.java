ac3b45d6028dad12aadd5c2af27f66ec5a18c6a9
Officialravi888 <youremail@yourdomain.com>
2023-02-25 22:36:54 +0530

    projectH

---
+++ Downloads/hibernate_new/hibernate_project_complet/src/main/java/org/example/Test.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class Test {
+    private static SessionFactory sessionFactory = null;
+
+    static {
+        try {
+            loadSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        //configuration.addResource("Employee.hbm.xml");
+        configuration.addAnnotatedClass(org.example.Employee.class);
+        ServiceRegistry serviceRegistry=new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+    public static Session getSession() throws HibernateException{
+        Session session=null;
+        try{
+            session=sessionFactory.openSession();
+        }catch (Exception e){
+            System.err.println("Exception while is opening");
+            e.printStackTrace();
+        }
+        return session;
+    }
+
+    public static void main(String[] args) {
+        Session session=null;
+        Transaction txn=null;
+        try{
+            session = getSession();
+            txn = session.beginTransaction();
+            Employee user = new Employee("tom",24);
+            session.save(user);
+            txn.commit();
+        }catch (Exception e){
+            if (txn !=null)
+                txn.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+    }
+}
